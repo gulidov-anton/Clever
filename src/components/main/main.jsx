933160edef6d268673530c272f7760785971a339
Anton <gulidov.antonio@gmail.ru>
2021-06-17 09:54:26 +0300

    Тудуха

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Cards } from "../cards";
+import { Todo } from "../todo";
 
 import styles from "./main.module.css";
 
@@ -9,6 +10,7 @@ export class Main extends React.Component {
     return (
       <div className={styles.main}>
         <Cards />
+        <Todo />
       </div>
     );
   }
